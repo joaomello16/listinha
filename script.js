ed046cd2e0dedc9a89fa30db5f49cb23fe398e09
João Pedro Vieira De Mello <joaomello1618@gmail.com>
2025-05-09 19:42:12 -0300

    at
    att

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tarefas = ['tarefa-1','tarefa-2','tarefa3']
+const tarefas = []
 
 function add(){
     const input = document.querySelector('input')
@@ -13,14 +13,25 @@ function add(){
 function render(){
   const ul = document.querySelector('ul')
     ul.innerHTML = null
-  for (t of tarefas){
+  tarefas.forEach(function(t){
     const li = document.createElement('li')
     li.innerText = t
     ul.appendChild(li)
-  }
+
+    const button = document.createElement('button')
+    button.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+    li.appendChild(button)
+
+  })
 
     
 }
+
+document.addEventListener('keydown', function(event){
+  if(event.key === 'Enter'){
+    add()
+  }
+})
 
 render()
 
