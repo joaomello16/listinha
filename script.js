203f50d0123d491d5362b7ab51e974462209147f
João Pedro Vieira De Mello <joaomello1618@gmail.com>
2025-05-12 16:40:24 -0300

    Update script.js
    at

--- script.js
+++ script.js
@@ -10,18 +10,26 @@ function add(){
 
 }
 
+function remove(index){
+  tarefas.splice(index, 1)
+  render()
+}
+
 function render(){
   const ul = document.querySelector('ul')
     ul.innerHTML = null
-  tarefas.forEach(function(t){
+  tarefas.forEach(function(t, index){
     const li = document.createElement('li')
     li.innerText = t
     ul.appendChild(li)
 
     const button = document.createElement('button')
     button.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+    button.onclick = function(){
+      remove(index)
+    }
+    
     li.appendChild(button)
-
   })
 
     
