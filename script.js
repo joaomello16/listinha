fc1ee18413c42226f5222ac40c279d354a37a5fb
João Pedro Vieira De Mello <joaomello1618@gmail.com>
2025-05-15 15:43:11 -0300

    zz

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
+
 const tarefas = []
 
 function add(){
-    const input = document.querySelector('input')
-    const tarefa = input.value
-    if(tarefa.trim() === '') return
-    tarefas.push(tarefa)
-    input.value = ''
-    render()  
-
+  const input = document.querySelector('input')
+  const tarefa = input.value
+  if( tarefa.trim() === '')return
+  tarefas.push(tarefa)
+  input.value = ''
+  render()
 }
 
 function remove(index){
-  tarefas.splice(index, 1)
+  tarefas.splice(index,1)
   render()
 }
 
 function render(){
   const ul = document.querySelector('ul')
-    ul.innerHTML = null
+  ul.innerHTML = null
   tarefas.forEach(function(t, index){
     const li = document.createElement('li')
     li.innerText = t
     ul.appendChild(li)
 
     const button = document.createElement('button')
-    button.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
-    button.onclick(function())
+    button.innerHTML = 'X'
+    button.onclick = function(){
+      remove(index)
+    }
     li.appendChild(button)
-
   })
-
-    
 }
-
-document.addEventListener('keydown', function(event){
-  if(event.key === 'Enter'){
-    add()
-  }
-})
-
-render()
-
