1ec0ae11e5ed48835b887ee671130efeff5c72da
João Pedro Vieira De Mello <joaomello1618@gmail.com>
2025-07-01 16:11:28 -0300

    att

--- script.js
+++ script.js
@@ -2,39 +2,37 @@
 const tarefas = JSON.parse(localStorage.getItem('tarefas'))
 
 function add(){
-  const input = document.querySelector('input')
-  const tarefa = input.value
-  if(tarefa.trim() === '')return
-  tarefas.push(tarefa)
-  console.log(tarefas)
-  input.value = ''
-  salvar()
-  render()
+    const input = document.querySelector('input')
+    const tarefa = input.value
+    if(tarefa.trim() == '')return
+    tarefas.push(tarefa)
+    console.log(tarefas)
+    input.value = ''
+    salvar()
+    render()
 }
 
 function salvar(){
-  localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 }
-
 function render(){
-  const ul = document.querySelector('ul')
-  ul.innerHTML = null
-  tarefas.forEach(function(t, index){
-    const li = document.createElement('li')
-    const button = document.createElement('button')
+    const ul = document.querySelector('ul')
+    ul.innerHTML = null
+    tarefas.forEach(function(t, index){
+        const li = document.createElement('li')
+        const button = document.createElement('button')
+        button.innerHTML = "Excluir"
+        button.className = 'btnExcluir'
+        li.innerText = t
 
-    button.innerHTML = 'Excluir'
-    button.className = 'btnExcluir'
-    li.innerText = t
+        button.onclick = function(){
+            tarefas.splice(index, 1)
+            salvar()
+            render()
+        }
 
-    button.onclick = function(){
-      tarefas.splice(index, 1)
-      salvar()
-      render()
-    }
-
-    ul.appendChild(li)
-    li.appendChild(button)
-  })
+        ul.appendChild(li)
+        li.appendChild(button)
+    })
 }
 render()
